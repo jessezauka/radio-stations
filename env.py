1afa55567224cbd090e6890f0de5836028a3dd4f
Jesse Zauka <ceslovas@gmail.com>
2020-06-07 10:14:27 +0100

    Removed gitignore file

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["MONGO_URI"] = "mongodb+srv://Ceslovas:Microsoftsux1984@cookbook-jqvqs.mongodb.net/radio-stations?retryWrites=true&w=majority"
-
-MONGO_URI = "mongodb+srv://Ceslovas:Microsoftsux1984@cookbook-jqvqs.mongodb.net/radio-stations?retryWrites=true&w=majority"
