2884c8cfe7f41f8d2e4e1215ac58ea53047be925
Jesse Zauka <ceslovas@gmail.com>
2020-06-09 05:20:46 +0100

    Fix contact us form

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,16 +1,19 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "contact_us", {
+    emailjs.send("gmail", "contact", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
     })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                contactForm.name.value = ""
+                contactForm.emailaddress.value = ""
+                contactForm.projectsummary.value = ""
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
     return false;  // To block from loading a new page
 }
